622a6922d9fd3350bd17afa66eb3b26b975dc2de
Vishal Sharma <vishalsharma15122003@gmail.com>
2024-03-25 17:04:43 +0530

    Auth implemented

--- server/index.js
+++ server/index.js
@@ -1,16 +1,19 @@
 const express = require("express");
+const db = require("./config/database")
+
 const app = express();
-const data = "this is a temporary data";
 // load evv file
 require('dotenv').config();
 
 
 
 const Profile = require('./router/Profile')
-
-
 const PORT = process.env.PORT || 3000;
+db.connect();
+
+app.use(express.json());
 app.use('/api' , Profile);
+
 
 app.get('/' ,(req,res)=>{
     res.send("This is home page");
---
+++ server/model/User.js
@@ -0,0 +1,22 @@
+const mongoose = require('mongoose')
+
+const UserModel = new mongoose.Schema({
+    email:{
+        type: String,
+        required: true
+    },
+    password:{
+        type: String,
+        required: true
+    },
+    name: {
+        type: String,
+        required: true
+    },
+    number:{
+        type: Number,
+        required: true,
+    }
+})
+
+module.exports = mongoose.model("User",UserModel);
