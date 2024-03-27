db0b67d4ad4935eddf826a9d77db41296ec7cdc5
Vishal Sharma <vishalsharma15122003@gmail.com>
2024-03-27 11:55:54 +0530

    temp commit

--- frontend/src/Auth/Login.js
+++ frontend/src/Auth/Login.js
@@ -1,7 +1,18 @@
-import React from 'react'
+import React, { useState } from 'react'
 import GoogleButton from 'react-google-button'
 
 const Login = () => {
+  const [email,setEmail] = useState();
+  const [password,setPassword] = useState();
+  const LoginHandler = () => {
+    if(!email || !password){
+      return ;
+    }
+    else{
+      
+    }
+  }
+
   return (
     <div className='h-screen w-screen p-8 flex justify-center items-center'>
         <div className='h-3/4 w-96 bg-white shadow-2xl p-4 border rounded-md'>
@@ -15,16 +26,20 @@ const Login = () => {
 
           <div className='flex gap-3 flex-col'>
             <label>Email</label>
-            <input className='border p-4' type="text" placeholder='Enter Username' />
+            <input className='border p-4' type="text" placeholder='Enter Username'
+            onChange={(event) => {setEmail(event.target.value)}} />
           </div>
 
           <div className='flex gap-3 flex-col mt-8'>
             <label>Password</label>
-            <input className='border p-4' type="text" placeholder='Enter Username' />
+            <input className='border p-4' type="text" placeholder='Enter Username'
+            onChange={(event)=>{
+              setPassword(event.target.value)}} />
           </div>
 
           <div className='flex justify-center'>
-            <button className='border p-2 mt-4 bg-blue-500 text-white rounded-md'>Login</button>
+            <button className='border p-2 mt-4 bg-blue-500 text-white rounded-md'
+            onClick={LoginHandler}>Login</button>
           </div>
         </div>
     </div>
